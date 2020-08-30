45c4d80703137c21b73d4b42648ab0702bb3f705
owenThurm <owen.p.thurm@gmail.com>
2020-08-29 21:40:37 -0400

    Imports dependencies, bypasses ssl, and initializes chatbot

---
+++ chatbot.py
@@ -0,0 +1,17 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from chatterbot.conversation import Statement
+import nltk
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+Hugo = ChatBot(name="Hugo",
+                      read_only = False,
+                      logic_adapters = ["chatterbot.logic.BestMatch"],
+                      storage_adapter = "chatterbot.storage.SQLStorageAdapter")
